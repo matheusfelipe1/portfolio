9c317cf6bf3c48afa9cd67b26fef157f9cf8345a
Matheus Felipe Silva Santos <matheusfelipesilvasantos@Matheuss-Mac-mini.local>
2022-12-23 20:15:55 -0300

    inicio e assets

---
+++ src/app/views/initialpage/initialpage.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-initialpage',
+  templateUrl: './initialpage.component.html',
+  styleUrls: ['./initialpage.component.css']
+})
+export class InitialpageComponent {
+
+  
+
+
+  constructor() {}
+
+}
