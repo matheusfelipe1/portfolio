b25fd74f45e3c18c627a8f262c43eb1202c73208
Matheus Felipe Silva Santos <matheusfelipesilvasantos@Matheuss-Mac-mini.local>
2022-12-23 20:16:22 -0300

    inicio e assets

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InitialpageComponent } from './views/initialpage/initialpage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: InitialpageComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InitialpageComponent } from './views/initialpage/initialpage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InitialpageComponent
   ],
   imports: [
     BrowserModule,
